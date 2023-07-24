68de09f8ee6b2148b441186b59d3892a8b95d080
karan <110165652+BG4U-KARAN@users.noreply.github.com>
2023-07-24 19:30:20 +0530

    Delete src directory

--- src/database/schemas/register.js
+++
@@ -1,9 +0,0 @@
-const {model, Schema} = require("mongoose");
-
-const register = new Schema({
-    Id: Number,
-    email: String,
-    password: String,
-});
-
-module.exports = model("register", register)
